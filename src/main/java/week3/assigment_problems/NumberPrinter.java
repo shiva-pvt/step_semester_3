bacb243c92f5286a73782ba9d515ab91ab17460d
shiva2007-web <gnanashiva07@gmail.com>
2026-09-23 14:57:37 +0530

    Remove code from main to follow structure guide

--- src/main/java/week3/assigment_problems/NumberPrinter.java
+++
@@ -1,9 +0,0 @@
-package week3.assigment_problems;
-
-public class NumberPrinter {
-    public void printNumbersUpToN(int n) {
-        for (int i = 1; i <= n; i++) {
-            System.out.println(i);
-        }
-    }
-}
